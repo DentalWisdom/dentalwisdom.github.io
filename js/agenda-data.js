fb51a4383aa7c73a383f3dd945841fe9e7b3728e
Ben <ben@dentalwisdom.org>
2026-06-18 04:47:40 +0000

    Remove .html from URLs: move pages to folder/index.html, update all internal links

--- js/agenda-data.js
+++ js/agenda-data.js
@@ -34,14 +34,14 @@ window.AGENDA_DATA = [
   { day: "Thursday", time: "8:00 – 8:45 AM", title: "Breakfast", speaker: "", location: "OVO at The Altair" },
   { day: "Thursday", time: "8:15 – 8:45 AM", title: "Registration", speaker: "", location: "" },
   { day: "Thursday", time: "8:45 – 9:00 AM", title: "Welcoming Remarks", speaker: "Dr. Lisa (Greenstein) Sokol", location: "Shul", showInCEView: true },
-  { day: "Thursday", time: "9:00 – 10:00 AM", title: "Product Development – The Story of TheraBreath", speaker: "Dr. Harold Katz", speakerUrl: "conference-speakers.html#speaker-harold-katz", location: "Shul", ce: true, ceCredits: 1 },
-  { day: "Thursday", time: "10:00 AM – 12:00 PM", title: "Designing Smiles That Last: Modern Prosthodontic Strategies for Predictable, Aesthetic & Functional Excellence", speaker: "Dr. Daniel Greenbaum", speakerUrl: "conference-speakers.html#speaker-daniel-greenbaum", location: "Shul", ce: true, ceCredits: 2 },
+  { day: "Thursday", time: "9:00 – 10:00 AM", title: "Product Development – The Story of TheraBreath", speaker: "Dr. Harold Katz", speakerUrl: "/conference-speakers#speaker-harold-katz", location: "Shul", ce: true, ceCredits: 1 },
+  { day: "Thursday", time: "10:00 AM – 12:00 PM", title: "Designing Smiles That Last: Modern Prosthodontic Strategies for Predictable, Aesthetic & Functional Excellence", speaker: "Dr. Daniel Greenbaum", speakerUrl: "/conference-speakers#speaker-daniel-greenbaum", location: "Shul", ce: true, ceCredits: 2 },
   { day: "Thursday", time: "12:00 – 2:00 PM", title: "Lunch", speaker: "", location: "OVO at The Altair" },
-  { day: "Thursday", time: "2:00 – 4:00 PM", title: "'Hopeless' to Heroic: Modern Approaches to Periodontal Regeneration and Plastic Surgery", speaker: "Dr. Sean Ference", speakerUrl: "conference-speakers.html#speaker-sean-ference", location: "Shul", ce: true, ceCredits: 2 },
+  { day: "Thursday", time: "2:00 – 4:00 PM", title: "'Hopeless' to Heroic: Modern Approaches to Periodontal Regeneration and Plastic Surgery", speaker: "Dr. Sean Ference", speakerUrl: "/conference-speakers#speaker-sean-ference", location: "Shul", ce: true, ceCredits: 2 },
   { day: "Thursday", time: "4:00 – 6:00 PM", title: "Lecture Title TBD", speaker: "Speaker TBD", location: "Shul", ce: true, ceCredits: 2 },
   { day: "Thursday", time: "6:00 – 6:30 PM", title: "Break (Mincha & Maariv)", speaker: "", location: "Shul" },
   { day: "Thursday", time: "6:30 – 7:30 PM", title: "Lecture Title TBD", speaker: "Speaker TBD", location: "Shul", ce: true, ceCredits: 1 },
-  { day: "Thursday", time: "8:00 PM", title: "Welcome Party", speaker: "", location: "Atrium", sponsor: "LiveWell Capital", sponsorUrl: "conference-sponsors.html" },
+  { day: "Thursday", time: "8:00 PM", title: "Welcome Party", speaker: "", location: "Atrium", sponsor: "LiveWell Capital", sponsorUrl: "/conference-sponsors" },
 
   // ---------------- Friday ----------------
   { day: "Friday", time: "6:00 – 7:00 AM", title: "Daf Yomi Shiur", speaker: "", location: "Library" },
@@ -55,7 +55,7 @@ window.AGENDA_DATA = [
   { day: "Friday", time: "4:00 – 5:00 PM", title: "Lecture Title TBD", speaker: "Speaker TBD", location: "Shul", ce: true, ceCredits: 1 },
   { day: "Friday", time: "6:00 PM", title: "Mincha & Maariv", speaker: "", location: "Shul" },
   { day: "Friday", time: "8:00 PM", title: "Dinner", speaker: "", location: "OVO at The Altair" },
-  { day: "Friday", time: "10:30 PM", title: "Oneg", speaker: "", location: "Shul", sponsor: "Crown Catapult", sponsorUrl: "conference-sponsors.html" },
+  { day: "Friday", time: "10:30 PM", title: "Oneg", speaker: "", location: "Shul", sponsor: "Crown Catapult", sponsorUrl: "/conference-sponsors" },
 
   // ---------------- Shabbos ----------------
   { day: "Shabbos", time: "8:15 – 9:00 AM", title: "Daf Yomi Shiur", speaker: "", location: "Library" },
@@ -64,10 +64,10 @@ window.AGENDA_DATA = [
   { day: "Shabbos", time: "11:00 AM", title: "Dvar Torah", speaker: "", location: "Shul" },
   { day: "Shabbos", time: "12:00 PM", title: "Lunch", speaker: "", location: "OVO at The Altair" },
   { day: "Shabbos", time: "3:00 – 4:00 PM", title: "Lecture Title TBD", speaker: "Speaker TBD", location: "Shul", ce: true, ceCredits: 1 },
-  { day: "Shabbos", time: "4:00 – 5:00 PM", title: "Life Insurance: Bitachon or Hishtadlus? Navigating the Halachic Sources", speaker: "Sam Waller, CFP®", speakerUrl: "conference-speakers.html#speaker-sam-waller", location: "Shul", ce: true, ceCredits: 1 },
+  { day: "Shabbos", time: "4:00 – 5:00 PM", title: "Life Insurance: Bitachon or Hishtadlus? Navigating the Halachic Sources", speaker: "Sam Waller, CFP®", speakerUrl: "/conference-speakers#speaker-sam-waller", location: "Shul", ce: true, ceCredits: 1 },
   { day: "Shabbos", time: "5:00 – 6:00 PM", title: "Lecture Title TBD", speaker: "Speaker TBD", location: "Shul", ce: true, ceCredits: 1 },
   { day: "Shabbos", time: "6:00 PM", title: "Mincha", speaker: "", location: "Shul" },
-  { day: "Shabbos", time: "6:30 PM", title: "Shalosh Seudos & Dental Halacha Shiur", speaker: "Rabbi Dr. David J. Katz", speakerUrl: "conference-speakers.html#speaker-rabbi-david-katz", location: "Shul", ce: true, ceCredits: 1, sponsor: "Touro College of Dental Medicine", sponsorUrl: "conference-sponsors.html" },
+  { day: "Shabbos", time: "6:30 PM", title: "Shalosh Seudos & Dental Halacha Shiur", speaker: "Rabbi Dr. David J. Katz", speakerUrl: "/conference-speakers#speaker-rabbi-david-katz", location: "Shul", ce: true, ceCredits: 1, sponsor: "Touro College of Dental Medicine", sponsorUrl: "/conference-sponsors" },
   { day: "Shabbos", time: "7:00 PM", title: "Maariv", speaker: "", location: "Shul" },
   { day: "Shabbos", time: "8:30 PM", title: "Closing Party", speaker: "", location: "Atrium" }
 ];

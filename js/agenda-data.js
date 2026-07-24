da0cd6a0691d269f0158c54e0bc5a3aec54b37d1
Ben <ben@dentalwisdom.org>
2026-07-24 10:34:53 -0600

    Fri 10:30 PM title: Oneg & Shiur prefix

--- js/agenda-data.js
+++ js/agenda-data.js
@@ -92,7 +92,7 @@ window.AGENDA_DATA = [
       { name: "CG Insurance Group", url: "/conference-sponsors" }
     ], sponsorLabel: "Chazanus sponsored by" },
   { day: "Friday", time: "8:00 PM", title: "Shabbos Dinner", speaker: "", location: "OVO at The Altair" },
-  { day: "Friday", time: "10:30 PM", title: "From Isolation to Achdus: Strengthening the Jewish Dental Community", speaker: "To Be Announced Soon", location: "Shul", ce: true, ceCredits: 1.5, sponsor: "Crown Catapult", sponsorUrl: "/conference-sponsors" },
+  { day: "Friday", time: "10:30 PM", title: "Oneg & Shiur: From Isolation to Achdus - Strengthening the Jewish Dental Community", speaker: "To Be Announced Soon", location: "Shul", ce: true, ceCredits: 1.5, sponsor: "Crown Catapult", sponsorUrl: "/conference-sponsors" },
 
   // ---------------- Shabbos ----------------
   { day: "Shabbos", time: "8:15 AM", title: "Pre-Davening Shiur", speaker: "Dr. Samuel Schuster (General Dentist)", location: "Library", ce: true, ceCredits: 1 },
